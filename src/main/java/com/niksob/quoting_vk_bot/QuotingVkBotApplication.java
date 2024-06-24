752933500a2e48a59b1f0daa6ae46c386754e392
nicksobolev <nikitasob23@gmail.com>
2024-06-24 19:35:00 +0300

    Инициализация проекта

---
+++ src/main/java/com/niksob/quoting_vk_bot/QuotingVkBotApplication.java
@@ -0,0 +1,13 @@
+package com.niksob.quoting_vk_bot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class QuotingVkBotApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(QuotingVkBotApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/niksob/quoting_vk_bot/QuotingVkBotApplicationTests.java
@@ -0,0 +1,13 @@
+package com.niksob.quoting_vk_bot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class QuotingVkBotApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
