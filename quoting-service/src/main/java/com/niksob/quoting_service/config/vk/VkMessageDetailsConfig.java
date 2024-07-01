9148e0b50cf3aac92aad5fc664d5a644fcc758d4
nicksobolev <nikitasob23@gmail.com>
2024-07-01 23:14:21 +0300

    Удалил лишние геттеры и сеттеры в классе конфигурации. Они и так идут уже вместе с @Data

--- quoting-service/src/main/java/com/niksob/quoting_service/config/vk/VkMessageDetailsConfig.java
+++ quoting-service/src/main/java/com/niksob/quoting_service/config/vk/VkMessageDetailsConfig.java
@@ -1,16 +1,12 @@
 package com.niksob.quoting_service.config.vk;
 
 import lombok.Data;
-import lombok.Getter;
-import lombok.Setter;
 import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
-@Data
-@Getter
-@Setter
 @ConfigurationProperties(prefix = "vk")
+@Data
 public class VkMessageDetailsConfig {
     private String token;
     private String apiVersion;
